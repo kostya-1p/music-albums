949aa513cabb35092e987d81ae18dbd7fcbb4fd3
Pavel Kostitsyn <paulkostitsyn@gmail.com>
2022-09-09 10:21:46 +0400

    Group routes

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\AlbumController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::group(['controller' => AlbumController::class, 'middleware' => 'auth', 'prefix' => 'album'], function () {
+    Route::get('/create', 'getCreatePage')->name('createAlbumPage');
+    Route::get('/edit/{id}', 'getEditPage')->name('editAlbumPage')->whereNumber('id');
+    Route::post('/create', 'addAlbum')->name('addAlbum');
+    Route::post('/edit', 'editAlbum')->name('editAlbum');
+    Route::post('/delete', 'deleteAlbum')->name('deleteAlbum');
+});
 
-Route::get('/', [\App\Http\Controllers\AlbumController::class, 'showAllAlbums'])->
-name('albums');
-
-Route::get('/album/create', [\App\Http\Controllers\AlbumController::class, 'getCreatePage'])->
-name('createAlbumPage')->middleware('auth');
-
-Route::get('/album/edit/{id}', [\App\Http\Controllers\AlbumController::class, 'getEditPage'])->
-name('editAlbumPage')->middleware('auth')->whereNumber('id');
-
-Route::post('/album/create', [\App\Http\Controllers\AlbumController::class, 'addAlbum'])->
-name('addAlbum')->middleware('auth');
-
-Route::post('/album/edit', [\App\Http\Controllers\AlbumController::class, 'editAlbum'])->
-name('editAlbum')->middleware('auth');
-
-Route::post('/album/delete', [\App\Http\Controllers\AlbumController::class, 'deleteAlbum'])->
-name('deleteAlbum')->middleware('auth');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-Route::get('/search/{albumName}', [\App\Http\Controllers\AlbumController::class, 'searchAlbumByName'])->
-name('search');
-Route::get('/search_description/{albumName}/{artistName}', [\App\Http\Controllers\AlbumController::class, 'getAlbumDescription'])->
-name('albumInfo');
+Route::controller(AlbumController::class)->group(function () {
+    Route::get('/', 'showAllAlbums')->name('albums');
+    Route::get('/search/{albumName}', 'searchAlbumByName')->name('search');
+    Route::get('/search_description/{albumName}/{artistName}', 'getAlbumDescription')->
+    name('albumInfo');
+});
 
 require __DIR__ . '/auth.php';
